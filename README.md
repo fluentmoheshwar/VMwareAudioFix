8ea847af9c18e720dc76f69cb8ae7cd6a2aab7f9
Moheshwar Amarnath Biswas <fluentmoheshwar@outlook.com>
2023-10-19 01:44:09 +0600

    Init commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# VMwareAudioFix
+
+Eliminate VMware sound distortion that occurs when:
+
+- VMware is Workstation version 16 or above
+- Host OS is Windows 10 or above
+- Guest OS is Windows NT 5.x or earlier (e.g. Windows XP)
+- Virtual Machine Platform, Application Guard or Hyper-V enabled.
+
+## Installation
+
+1. Install VMware Tools on Guest OS
+1. Install [Sound Blaster Driver](https://dl.malwarewatch.org/drivers/SoundBlasterPCI98.zip) on Guest OS if you're using Windows 98
+1. [Download VirtualBox](https://www.virtualbox.org/)
+1. Click on the code button and download zip.
+1. Extract the archive then double click on install.bat and follow the instructions.
---
+++ install.bat
@@ -0,0 +1,14 @@
+@echo off
+title VMwareAudioFix Installer
+echo Make sure VirtualBox is installed.
+echo.
+echo Installing VMwareAudioFix...
+set PATH=C:\Program Files\Oracle\VirtualBox;%PATH%
+vboxmanage unregistervm VMAudioBack
+vboxmanage unregistervm VMwareAudioFix
+mkdir "%USERPROFILE%\VirtualBox VMs\VMwareAudioFix"
+copy /y "%~dp0\VMwareAudioFix.vbox" "%USERPROFILE%\VirtualBox VMs\VMwareAudioFix\VMwareAudioFix.vbox"
+vboxmanage registervm "%USERPROFILE%\VirtualBox VMs\VMwareAudioFix\VMwareAudioFix.vbox"
+echo Starting VMwareAudioFix...
+echo You can also start manually later from VirtualBox. Make sure to headless start.
+VBoxHeadless.exe -s VMAudioBack -start-paused
